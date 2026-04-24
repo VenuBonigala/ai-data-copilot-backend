bd8c6cb5c5abbf4af7c9356e61b74280b49c5b9f
VenuBonigala <bonigalavenuvaraprasad@gmail.com>
2026-04-25 03:21:55 +0530

    Deployment failure debugging
    Log the DATABASE_URL for debugging purposes.

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
-
+print("DATABASE_URL:", DATABASE_URL)
 forbidden_keywords = ["DROP", "DELETE", "TRUNCATE", "ALTER"]
 
 def is_safe_query(query: str):
