b96dfc1d06906a656b8e3af0f2fd22bf1ed573aa
VenuBonigala <bonigalavenuvaraprasad@gmail.com>
2026-04-25 05:06:44 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
       CORSMiddleware,
-      allow_origins=["https://ai-data-copilot-chi8y7sfr-venu-bonigalas-projects.vercel.app/"],
+      allow_origins=["https://ai-data-copilot.vercel.app/"],
       allow_credentials=True,
       allow_methods=["*"],
       allow_headers=["*"],
